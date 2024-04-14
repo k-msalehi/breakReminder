0e71deb63028cfd9911ddc30f2688a3bd2533d09
Mohammad Salehi <mds1121@yahoo.com>
2024-04-14 23:40:15 +0330

    Update breakReminder.py

--- src/breakReminder.py
+++ src/breakReminder.py
@@ -5,14 +5,17 @@ import time
 import os
 import pygame
 from datetime import datetime
+import json
+import random
 
 class ReminderApp:
-    def __init__(self, root, interval, play_music):
+    def __init__(self, root, interval, play_music, music_file, reminder_messages):
         self.root = root
         pygame.mixer.init()
         self.interval = interval  # Interval in seconds
         self.play_music = play_music
-        self.music_file = "music.mp3"
+        self.music_file = music_file
+        self.reminder_messages = reminder_messages
         self.reminder_active = False
         self.next_reminder_time = time.time() + self.interval  # Initialize the next reminder time
         self.start_timer()
@@ -29,7 +32,7 @@ class ReminderApp:
                 self.show_reminder()
                 # Wait for the reminder window to close before calculating the next reminder time
                 while self.reminder_active:
-                    time.sleep(1)  # Short sleep to prevent high CPU usage
+                    time.sleep(2)  # Short sleep to prevent high CPU usage (don't ask me why I use 2 seconds, it just is)
                 # Set the next reminder time
                 self.next_reminder_time = time.time() + self.interval
 
@@ -40,15 +43,18 @@ class ReminderApp:
         self.reminder_window.title("breakReminder")
         self.reminder_window.state('zoomed')
         self.reminder_window.configure(background='black')
-        label = tk.Label(self.reminder_window, text="Time for a break!", fg="white", bg="black", font=("Helvetica", 32))
+        
+        # Select a random message from the list
+        random_message = random.choice(self.reminder_messages)
+        label = tk.Label(self.reminder_window, text=random_message, fg="white", bg="black", font=("Helvetica", 32))
         label.pack(expand=True)
+        
         self.reminder_window.attributes('-topmost', True)  # Make window topmost
         self.reminder_window.protocol("WM_DELETE_WINDOW", self.on_close_reminder)
 
         if self.play_music and os.path.exists(self.music_file):
             pygame.mixer.music.load(self.music_file)
             pygame.mixer.music.play()
-
 
     def on_close_reminder(self):
         if pygame.mixer.get_init():
@@ -60,15 +66,21 @@ class ReminderApp:
 
 
 def main():
-    interval = int(input("Enter the time in minutes for the reminder (default 35): ") or 35) * 60
-    play_music = input("Do you want to play music (y/n)? ").lower().startswith('y')
+    with open('config.json') as f:
+        config = json.load(f)
+
+    interval = int(config.get("reminderInMinutes", 35)) * 60
+    music_path = config.get("musicPath", "music.mp3")
+    play_music = config.get("playMusic", "no").lower().startswith('y')
+    reminder_messages = config.get("reminderMessages", ["Time for a break!"])
 
     root = tk.Tk()
     root.withdraw()
 
-    app = ReminderApp(root, interval, play_music)
-    print("Github repo: https://github.com/k-msalehi/breakReminder")
-    print("OK, breakReminder started at: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S') )
+    app = ReminderApp(root, interval, play_music, music_path, reminder_messages)
+    print("Project Github repo: https://github.com/k-msalehi/breakReminder")
+    print("OK, breakReminder started at: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print("")
     root.mainloop()
 
     pygame.mixer.quit()
